68e6a69c88e44ab23a424853ad0d93e4623bb11b
Emma Gray <e.gray@alumni.ubc.ca>
2019-07-12 23:49:55 -0700

    [minor] - clarified a comment

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@
 //      ^^^       ^^^
 
 // Notes on calibration protocol:
-// - bring wings in to min, set encoder val 0
+// - bring wings in to min, set encoder lapCount to 0
 // - bring wings out to max, counting encoder rotations. Save num rotations as max.
 
 // Code for Master-duino
@@ -21,24 +21,6 @@
 
 #define encoder_A A3 // some analog in pin
 #define encoder_B A5 // some analog in pin
-
-// Encoder A values
-int lapCount_A = 0;         // rev counter
-int minPos_A = 0;           // mechanical min position. Determined through calibration
-int maxPos_A = 1025;        // mechanical max position. Determined through calibration
-
-int lastPos_A = 0;          // for measuring actual velocity
-int position_A = 0;         // #revs * 1024 + current reading
-int speed_A = 0;
-
-// Encoder B values
-int lapCount_B = 0;         // rev counter
-int minPos_B = 0;           // mechanical min position. Determined through calibration
-int maxPos_B = 1025;        // mechanical max position. Determined through calibration
-
-int lastPos_B = 0;          // for measuring actual velocity
-int position_B = 0;         // #revs * 1024 + current reading
-int speed_B = 0;            // determined through calibration
 
 int currentSpeed = 300;                            // speed (Set Point)
 int targetSpeed = 0;                              // speed (actual value)
@@ -65,13 +47,14 @@ void setup() {
   pinMode(encoder_B, INPUT);
   md.init();
 
-  calibrate();
-  
+  // init both encoders
+
+  calibrate();  
 
 }
 
 void loop() {
-  
+
   // put your main code here, to run repeatedly:
   // while (not at max limit - safety factor)
   // start motion of A
