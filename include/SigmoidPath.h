91c41d0281fcb7cccb663a4a1d8ebadd565057d1
Emma Gray <e.gray@alumni.ubc.ca>
2019-07-13 22:51:30 -0700

    [major] - add sigmoid class

--- include/SigmoidPath.h
+++ include/SigmoidPath.h
@@ -1,16 +1,26 @@
 #ifndef SIGMOIDPATH_H
 #define SIGMOIDPATH_H
 
+struct Motor {
+    float minPos;
+    float maxPos;
+};
+
 class SigmoidPath{
     private:
-        int cascadeOffset = 5000;   // ms
-        int sigmoidLength = 60000;  // ms
-        int midpointPause = 1000;   // ms
-        int totalDuration = sigmoidLength * 2 + 6 * cascadeOffset + midpointPause;
+        int cascadeOffset;  // ms
+        int sigmoidLength;  // ms
+        int midpointPause;  // ms
+        int totalDuration;
+        Motor m0;
+        Motor m1;
+        
+        float clamp(float x, float startPoint, int endPoint);
 
     public:
         SigmoidPath(float minPos[], float maxPos[]);
-        int getPath();
+        // returns desired position givne time
+        float getNextPos(int motor, int time, bool isClosing);
 };
 
 #endif
--- src/SigmoidPath.cpp
+++ src/SigmoidPath.cpp
@@ -1,8 +1,55 @@
 #include <Arduino.h>
 #include <SigmoidPath.h>
 
-SigmoidPath::SigmoidPath(float minPos[], float maxPos[])
-{
+SigmoidPath::SigmoidPath(float minPos[], float maxPos[]){
+    cascadeOffset = 5000;
+    sigmoidLength = 60000;
+    midpointPause = 1000;
+
+    totalDuration = sigmoidLength * 2 + 6 * cascadeOffset + midpointPause;
     
-}
-    
+    m0.maxPos = maxPos[0];
+    m0.minPos = minPos[0];
+
+    m1.maxPos = maxPos[1];
+    m1.minPos = minPos[1];
+};
+
+float clamp(float x, float startPoint, int endPoint){
+    if (x < startPoint) x = startPoint;
+    if (x > endPoint) x = endPoint;
+    return x;
+};
+
+
+// Motor A is 0, Motor B is 1. 
+// Assume Motor A opens before Motor B, and Motor B closes before Motor A.
+// Assume time param is relative to start of full-motion loop timer
+float SigmoidPath::getNextPos(int motor, int time, bool isClosing){
+    int pos;
+    if(!isClosing){
+        // Generate opening paths based on motor params
+        switch(motor){
+            case 0:
+                pos = clamp((time - cascadeOffset) / sigmoidLength, 0, 1);
+                // Evaluate Hermite interpolation poluynomial
+                return (m0.maxPos - m0.minPos) * (pos * pos * (3 - 2 * pos)) + m0.minPos;
+            case 1:
+                pos = clamp(time / sigmoidLength, m1.minPos, m1.maxPos);
+                // Evaluate Hermite interpolation poluynomial
+                return (m1.maxPos - m1.minPos) * (pos * pos * (3 - 2 * pos)) + m1.maxPos;
+        }
+    } else {
+        // Generate closing paths based on motor params
+        switch(motor){
+            case 0:
+                pos = clamp((-1 * (time - sigmoidLength)) / sigmoidLength, m0.minPos, m0.maxPos);
+                // Evaluate Hermite interpolation poluynomial
+                return (m0.maxPos - m0.minPos) * (pos * pos * (3 - 2 * pos)) + m0.minPos;
+            case 1:
+                pos = clamp((-1 * (time - sigmoidLength - cascadeOffset)) / sigmoidLength, m1.minPos, m1.maxPos);
+                // Evaluate Hermite interpolation poluynomial
+                return (m1.maxPos - m1.minPos) * (pos * pos * (3 - 2 * pos)) + m0.minPos;
+        }
+    }
+};
