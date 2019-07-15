8ab90d16debba1fea90d563d46c24f227e8ec4a3
Emma Gray <e.gray@alumni.ubc.ca>
2019-07-14 19:57:36 -0700

    [minor] - bugfix

--- include/Calibration.h
+++ include/Calibration.h
@@ -28,9 +28,9 @@ class Calibration{
         int sigA;
 
         // Encoder objects
-        Encoder top = Encoder::Encoder(top_encoder);
-        Encoder bottom = Encoder::Encoder(bot_encoder);
-
+        Encoder top;
+        Encoder bottom;
+    
         // Pin for top/bottom toggle switch
         int toggle_pin;
     
